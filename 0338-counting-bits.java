47269427be75d06fbdded5b5b7c7f4095ea565c4
TransouL <tran_soul@163.com>
2021-03-03 06:07:28 +0800

    0338-counting-bits

---
+++ 0338-counting-bits.java
@@ -0,0 +1,17 @@
+class Solution {
+	public static int[] countBits(int num) {
+		int[] result = new int[num + 1];
+		if (num == 0) {
+			return result;
+		}
+		result[1] = 1;
+		for (int i = 2; i + 1 <= num; i += 2) {
+			result[i] = result[i / 2];
+			result[i + 1] = result[i] + 1;
+		}
+		if (num % 2 == 0) {
+			result[num] = result[num / 2];
+		}
+		return result;
+	}
+}
