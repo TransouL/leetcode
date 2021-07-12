278ff298df437efb175bffc46c8d1afdb6483325
TransouL <tran_soul@163.com>
2021-07-12 23:20:42 +0800

    0275-h-index-ii

---
+++ 0275-h-index-ii.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        n = len(citations)
+        left, right = 0, n - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if citations[mid] >= n - mid:
+                right = mid - 1
+            else:
+                left = mid + 1
+        return n - left
+
+
+solution = Solution()
+print(solution.hIndex([0, 1, 3, 5, 6]))
