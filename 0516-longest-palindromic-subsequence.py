c6b9114a054cadb03f5f0b5743700727f24b4427
TransouL <tran_soul@163.com>
2021-08-12 01:32:41 +0800

    0516-longest-palindromic-subsequence

---
+++ 0516-longest-palindromic-subsequence.py
@@ -0,0 +1,18 @@
+class Solution:
+    def longestPalindromeSubseq(self, s: str) -> int:
+        n = len(s)
+        dp = [[0] * n for _ in range(n)]
+
+        for j in range(n):
+            dp[j][j] = 1
+            for i in range(j - 1, -1, -1):
+                if s[i] == s[j]:
+                    dp[i][j] = dp[i + 1][j - 1] + 2
+                else:
+                    dp[i][j] = max(dp[i + 1][j], dp[i][j - 1])
+        return dp[0][n - 1]
+
+
+solution = Solution()
+print(solution.longestPalindromeSubseq("bbbab"))
+print(solution.longestPalindromeSubseq("cbbd"))
