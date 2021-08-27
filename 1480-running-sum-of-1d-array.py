245e3b4821593269183c71f99e4b1a0f81db59fb
TransouL <tran_soul@163.com>
2021-08-28 01:03:00 +0800

    1480-running-sum-of-1d-array

---
+++ 1480-running-sum-of-1d-array.py
@@ -0,0 +1,12 @@
+from typing import List
+
+
+class Solution:
+    def runningSum(self, nums: List[int]) -> List[int]:
+        for i in range(1, len(nums)):
+            nums[i] += nums[i - 1]
+        return nums
+
+
+solution = Solution()
+print(solution.runningSum([1, 2, 3, 4]))
