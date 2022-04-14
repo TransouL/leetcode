da3e29acf68039c888b1f16010ed833d1e9e7042
TransouL <tran_soul@163.com>
2022-04-15 01:38:01 +0800

    0385-mini-parser

---
+++ 0385-mini-parser.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <string>
+#include <stack>
+using namespace std;
+
+class Solution {
+public:
+	int index = 0;
+	NestedInteger deserialize(string s) {
+		if (s[0] != '[') {
+			return NestedInteger(stoi(s));
+		}
+		stack<NestedInteger> st;
+		int num = 0;
+		bool negative = false;
+		for (int i = 0; i < s.size(); i++) {
+			char c = s[i];
+			if (c == '-') {
+				negative = true;
+			}
+			else if (isdigit(c)) {
+				num = num * 10 + c - '0';
+			}
+			else if (c == '[') {
+				st.emplace(NestedInteger());
+			}
+			else if (c == ',' || c == ']') {
+				if (isdigit(s[i - 1])) {
+					if (negative) {
+						num *= -1;
+					}
+					st.top().add(NestedInteger(num));
+				}
+				num = 0;
+				negative = false;
+				if (c == ']' && st.size() > 1) {
+					NestedInteger ni = st.top();
+					st.pop();
+					st.top().add(ni);
+				}
+			}
+		}
+		return st.top();
+	}
+};
+
+int main(void){
+	Solution solution = Solution();
+	solution.deserialize("324");
+	solution.deserialize("[123,[456,[789]]]");
+}
