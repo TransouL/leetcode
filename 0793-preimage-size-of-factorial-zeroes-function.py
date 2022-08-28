ca9a5f4884c566ea0bff2aaada3f19c551676429
TransouL <tran_soul@163.com>
2022-08-28 21:47:52 +0800

    0793-preimage-size-of-factorial-zeroes-function.py

---
+++ 0793-preimage-size-of-factorial-zeroes-function.py
@@ -0,0 +1,22 @@
+from bisect import bisect_left
+
+
+class Solution:
+    def preimageSizeFZF(self, k: int) -> int:
+        def zeta(n: int) -> int:
+            res = 0
+            while n:
+                n //= 5
+                res += n
+            return res
+
+        def nx(k: int) -> int:
+            return bisect_left(range(5 * k), k, key=zeta)
+
+        return nx(k + 1) - nx(k)
+
+
+solution = Solution()
+print(solution.preimageSizeFZF(0))
+print(solution.preimageSizeFZF(5))
+print(solution.preimageSizeFZF(3))
