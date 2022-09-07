9c942ec0aecc0420ef79a7bfac0ba10b9a64943a
TransouL <tran_soul@163.com>
2022-09-08 06:53:44 +0800

    0667-beautiful-arrangement-ii.cpp

---
+++ 0667-beautiful-arrangement-ii.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+	vector<int> constructArray(int n, int k) {
+		vector<int> ans;
+		for (int i = 1; i < n - k; i++) {
+			ans.push_back(i);
+		}
+		for (int i = n - k, j = n; i <= j; ++i, --j) {
+			ans.push_back(i);
+			if (i != j) {
+				ans.push_back(j);
+			}
+		}
+		return ans;
+	}
+};
+
+template<typename T>
+void printVector(const vector<T> &v){
+	for (auto &e : v) cout << e << ", ";
+	cout << endl;
+}
+
+int main(void){
+	Solution solution = Solution();
+	printVector(solution.constructArray(3, 1));
+	printVector(solution.constructArray(3, 2));
+}
