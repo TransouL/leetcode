7a038573ed37e02df3400adaa7e4b7fd8666b530
TransouL <tran_soul@163.com>
2022-09-13 01:32:06 +0800

    0670-maximum-swap.cpp

---
+++ 0670-maximum-swap.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+	int maximumSwap(int num) {
+		vector<int> orig;
+		while (num) {
+			orig.emplace_back(num % 10);
+			num /= 10;
+		}
+		vector<int> sorted = orig;
+		sort(sorted.begin(), sorted.end());
+		int n = orig.size();
+		for (int i = n - 1; i >= 0; --i) {
+			if (orig[i] != sorted[i]) {
+				for (int j = 0; j < i; j++) {
+					if (orig[j] == sorted[i]) {
+						swap(orig[j], orig[i]);
+						break;
+					}
+				}
+				break;
+			}
+		}
+		int ans = 0;
+		for (int i = n - 1; i >= 0; --i) {
+			ans = ans * 10 + orig[i];
+		}
+		return ans;
+	}
+};
+
+int main(void){
+	Solution solution = Solution();
+	cout << solution.maximumSwap(2736) << endl;
+	cout << solution.maximumSwap(9973) << endl;
+}
