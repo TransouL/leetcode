2e709e8ced6c5ab9218c8dfd4180e2dec64465af
TransouL <tran_soul@163.com>
2022-10-03 19:24:44 +0800

    1784-check-if-binary-string-has-at-most-one-segment-of-ones.cpp

---
+++ 1784-check-if-binary-string-has-at-most-one-segment-of-ones.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+	bool checkOnesSegment(string s) {
+		int state = 0;
+		for (auto &ch : s) {
+			if (state == 0) {
+				if (ch == '1') {
+					state = 1;
+				}
+			}
+			else if (state == 1) {
+				if (ch == '0') {
+					state = 2;
+				}
+			}
+			else {
+				if (ch == '1') {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+};
+
+int main(void){
+	Solution solution = Solution();
+	cout << solution.checkOnesSegment("1001") << endl;
+	cout << solution.checkOnesSegment("110") << endl;
+}
