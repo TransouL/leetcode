42a60b81d27a883ebad5c5471e72fb750d50dbd6
TransouL <tran_soul@163.com>
2022-10-04 00:58:20 +0800

    0921-minimum-add-to-make-parentheses-valid.cpp

---
+++ 0921-minimum-add-to-make-parentheses-valid.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+	int minAddToMakeValid(string s) {
+		int ans = 0, left = 0;
+		for (auto &ch : s) {
+			if (ch == '(') {
+				++left;
+			}
+			else {
+				if (left > 0) {
+					--left;
+				}
+				else {
+					++ans;
+				}
+			}
+		}
+		return ans + left;
+	}
+};
+
+int main(void){
+	Solution solution = Solution();
+	cout << solution.minAddToMakeValid("())") << endl;
+	cout << solution.minAddToMakeValid("(((") << endl;
+	cout << solution.minAddToMakeValid("()))((") << endl;
+}
