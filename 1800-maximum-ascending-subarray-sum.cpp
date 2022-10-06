8ba2c9396e020fc79ac90a9e73e2c2fa81765249
TransouL <tran_soul@163.com>
2022-10-07 00:40:05 +0800

    1800-maximum-ascending-subarray-sum.cpp

---
+++ 1800-maximum-ascending-subarray-sum.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+	int maxAscendingSum(vector<int>& nums) {
+		int ans = 0, curr = 0, prev = 0;
+		for (auto &num : nums) {
+			if (num > prev) {
+				curr += num;
+			}
+			else {
+				ans = max(ans, curr);
+				curr = num;
+			}
+			prev = num;
+		}
+		return max(ans, curr);
+	}
+};
+
+int main(void){
+	Solution solution = Solution();
+	cout << solution.maxAscendingSum(vector<int> {10, 20, 30, 5, 10, 50}) << endl;
+	cout << solution.maxAscendingSum(vector<int> {10, 20, 30, 40, 50}) << endl;
+	cout << solution.maxAscendingSum(vector<int> {12, 17, 15, 13, 10, 11, 12}) << endl;
+	cout << solution.maxAscendingSum(vector<int> {100, 10, 1}) << endl;
+}
