e15f10db9a3996a70a7254e9d59b2067b18e7cdb
TransouL <tran_soul@163.com>
2022-10-14 22:54:31 +0800

    0940-distinct-subsequences-ii.cpp

---
+++ 0940-distinct-subsequences-ii.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+const int MOD = 1000000007;
+
+class Solution {
+public:
+	int distinctSubseqII(string s) {
+		vector<int> g(26, 0);
+		int n = s.size(), total = 0;
+		for (int i = 0; i < n; i++) {
+			int oi = s[i] - 'a';
+			int prev = g[oi];
+			g[oi] = (total + 1) % MOD;
+			total = ((total + g[oi] - prev) % MOD + MOD) % MOD;
+		}
+		return total;
+	}
+};
+
+int main(void){
+	Solution solution = Solution();
+	cout << solution.distinctSubseqII("abc") << endl;
+	cout << solution.distinctSubseqII("aba") << endl;
+	cout << solution.distinctSubseqII("aaa") << endl;
+}
