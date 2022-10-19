fed33fed8465e38541a90bad2073ac3f9ce14ac4
TransouL <tran_soul@163.com>
2022-10-20 07:28:21 +0800

    0779-k-th-symbol-in-grammar.cpp

---
+++ 0779-k-th-symbol-in-grammar.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+	int kthGrammar(int n, int k) {
+		k--;
+		int ans = 0;
+		while (k > 0) {
+			k &= k - 1;
+			ans ^= 1;
+		}
+		return ans;
+	}
+};
+
+int main(void) {
+	Solution solution = Solution();
+	cout << solution.kthGrammar(1, 1) << endl;
+	cout << solution.kthGrammar(2, 1) << endl;
+	cout << solution.kthGrammar(2, 2) << endl;
+}
