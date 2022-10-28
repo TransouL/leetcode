e64bcf83f103bf8351b80d74e792d4580941123c
TransouL <tran_soul@163.com>
2022-10-29 07:16:42 +0800

    1779-count-items-matching-a-rule.cpp

---
+++ 1779-count-items-matching-a-rule.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+	int countMatches(vector<vector<string>>& items, string ruleKey, string ruleValue) {
+		int keyId = ruleKey == "type" ? 0 : ruleKey == "color" ? 1 : 2;
+		int ans = 0;
+		for (auto item : items) {
+			if (ruleValue == item[keyId]) {
+				++ans;
+			}
+		}
+		return ans;
+	}
+};
+
+int main(void) {
+	Solution solution = Solution();
+	cout << solution.countMatches(vector<vector<string>>{ {"phone", "blue", "pixel"}, { "computer", "silver", "phone" }, { "phone", "gold", "iphone" }}, "type", "phone") << endl;
+}
+
