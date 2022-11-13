6ff7509f593ff3ed0330111e020f8be964605073
TransouL <tran_soul@163.com>
2022-11-13 23:37:47 +0800

    0791-custom-sort-string.cpp

---
+++ 0791-custom-sort-string.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+	string customSortString(string order, string s) {
+		int cnt[26] = {0};
+		for (auto &ch : s) {
+			++cnt[ch - 'a'];
+		}
+		string ans;
+		for (auto &ch : order) {
+			if (cnt[ch - 'a'] != 0) {
+				ans.append(cnt[ch - 'a'], ch);
+				cnt[ch - 'a'] = 0;
+			}
+		}
+		for (int i = 0; i < 26; i++) {
+			if (cnt[i] != 0) {
+				ans.append(cnt[i], 'a' + i);
+			}
+		}
+
+		return ans;
+	}
+};
+
+int main(void) {
+	Solution solution = Solution();
+	cout << solution.customSortString("cba", "abcd") << endl;
+	cout << solution.customSortString("cbafg", "abcd") << endl;
+}
