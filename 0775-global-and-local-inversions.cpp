f6ff35274d67d20b89655216278f8e34c0032113
TransouL <tran_soul@163.com>
2022-11-16 02:12:13 +0800

    0775-global-and-local-inversions.cpp

---
+++ 0775-global-and-local-inversions.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+	bool isIdealPermutation(vector<int>& nums) {
+		int n = nums.size(), minSuff = nums[n - 1];
+		for (int i = n - 3; i >= 0 ; --i) {
+			if (nums[i] > minSuff) {
+				return false;
+			}
+			minSuff = min(minSuff, nums[i + 1]);
+		}
+		return true;
+	}
+};
+
+int main(void) {
+	Solution solution = Solution();
+	cout << solution.isIdealPermutation(vector<int> {1, 0, 2}) << endl;
+	cout << solution.isIdealPermutation(vector<int> {1, 2, 0}) << endl;
+}
