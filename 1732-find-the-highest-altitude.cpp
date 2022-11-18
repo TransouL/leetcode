ce65c7884f2cc6ca154aaf9a3098db566ffed07c
TransouL <tran_soul@163.com>
2022-11-19 00:40:24 +0800

    1732-find-the-highest-altitude.cpp

---
+++ 1732-find-the-highest-altitude.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+	int largestAltitude(vector<int>& gain) {
+		int ans = 0, curr = 0;
+		for (auto &diff : gain) {
+			curr += diff;
+			ans = max(ans, curr);
+		}
+		return ans;
+	}
+};
+
+int main(void) {
+	Solution solution = Solution();
+	cout << solution.largestAltitude(vector<int> {-5, 1, 5, 0, -7}) << endl;
+	cout << solution.largestAltitude(vector<int> {-4, -3, -2, -1, 4, 3, 2}) << endl;
+}
