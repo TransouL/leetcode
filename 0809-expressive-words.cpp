2e768bd239fd3c30cfa1402d6147ffa531b82bc5
TransouL <tran_soul@163.com>
2022-11-25 01:09:55 +0800

    0809-expressive-words.cpp

---
+++ 0809-expressive-words.cpp
@@ -0,0 +1,57 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+	int expressiveWords(string s, vector<string>& words) {
+		vector<char> letters;
+		vector<int> stats;
+		char curr = 0, cnt = 0;
+		for (auto &ch : s) {
+			if (ch != curr){
+				letters.push_back(curr);
+				stats.push_back(cnt);
+				curr = ch;
+				cnt = 1;
+			}
+			else {
+				++cnt;
+			}
+		}
+		letters.push_back(curr);
+		stats.push_back(cnt);
+
+		int ans = 0;
+		for (auto &word : words) {
+			word += '0';
+			bool is = true;
+			int pos = 0;
+			char curr = 0, cnt = 0;
+			for (auto &ch : word) {
+				if (ch != curr){
+					if (pos >= letters.size() || curr != letters[pos] || cnt > stats[pos] || (stats[pos] < 3 && cnt != stats[pos])) {
+						is = false;
+						break;
+					}
+					curr = ch;
+					cnt = 1;
+					++pos;
+				}
+				else {
+					++cnt;
+				}
+			}
+			if (is && pos == letters.size()) {
+				++ans;
+			}
+		}
+		return ans;
+	}
+};
+
+int main(void) {
+	Solution solution = Solution();
+	cout << solution.expressiveWords("heeellooo", vector<string>{"hello", "hi", "helo"}) << endl;
+	cout << solution.expressiveWords("abcd", vector<string>{"abc"}) << endl;
+}
