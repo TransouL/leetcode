6f49612771ac4c6d46cd7d1f0d1ec561fde5bdcd
TransouL <tran_soul@163.com>
2022-11-28 15:25:14 +0800

    0813-largest-sum-of-averages.cpp

---
+++ 0813-largest-sum-of-averages.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+	double largestSumOfAverages(vector<int>& nums, int k) {
+		int n = nums.size();
+		vector<double> prefix(n + 1);
+		for (int i = 0; i < n; i++) {
+			prefix[i + 1] = prefix[i] + nums[i];
+		}
+		vector<double> dp(n + 1);
+		for (int i = 0; i <=n; i++) {
+			dp[i] = prefix[i] / i;
+		}
+		for (int j = 2; j <= k; j++) {
+			for (int i = n; i >= j; --i) {
+				for (int x = j - 1; x < i; x++) {
+					dp[i] = max(dp[i], dp[x] + (prefix[i] - prefix[x]) / (i - x));
+				}
+			}
+		}
+		return dp[n];
+	}
+};
+
+int main(void) {
+	Solution solution = Solution();
+	cout << solution.largestSumOfAverages(vector<int>{9, 1, 2, 3, 9}, 3) << endl;
+	cout << solution.largestSumOfAverages(vector<int>{1, 2, 3, 4, 5, 6, 7}, 4) << endl;
+}
