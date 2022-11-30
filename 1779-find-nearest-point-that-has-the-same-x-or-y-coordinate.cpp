f7db25f1681464f68a050c5c204cf96269549863
TransouL <tran_soul@163.com>
2022-12-01 04:38:32 +0800

    1779-find-nearest-point-that-has-the-same-x-or-y-coordinate.cpp

---
+++ 1779-find-nearest-point-that-has-the-same-x-or-y-coordinate.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+	int nearestValidPoint(int x, int y, vector<vector<int>>& points) {
+		int ans = -1, curr = 10001;
+		for (int i = 0; i < points.size(); i++) {
+			if (points[i][0] == x || points[i][1] == y) {
+				int tmp = abs(points[i][0] - x) + abs(points[i][1] - y);
+				if (tmp < curr) {
+					ans = i;
+					curr = tmp;
+				}
+			}
+		}
+		return ans;
+	}
+};
+
+int main(void) {
+	Solution solution = Solution();
+	cout << solution.nearestValidPoint(3, 4, vector<vector<int>> {{1, 2}, { 3, 1 }, { 2, 4 }, { 2, 3 }, { 4, 4 }}) << endl;
+	cout << solution.nearestValidPoint(3, 4, vector<vector<int>> {{ 3, 4 }}) << endl;
+	cout << solution.nearestValidPoint(3, 4, vector<vector<int>> {{ 2, 3 }}) << endl;
+}
