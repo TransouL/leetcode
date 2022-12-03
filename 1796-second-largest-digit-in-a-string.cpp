a2d883f95da324a0ac0ea6de11eb5dd99fa35b7e
TransouL <tran_soul@163.com>
2022-12-03 12:23:55 +0800

    1796-second-largest-digit-in-a-string.cpp

---
+++ 1796-second-largest-digit-in-a-string.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+	int secondHighest(string s) {
+		int max1 = -1, max2 = -1;
+		for (auto &ch : s) {
+			if (isdigit(ch)) {
+				int t = ch - '0';
+				if (t > max1) {
+					max2 = max1;
+					max1 = t;
+				}
+				else if (t != max1 && t > max2) {
+					max2 = t;
+				}
+			}
+		}
+		return max2;
+	}
+};
+
+int main(void) {
+	Solution solution = Solution();
+	cout << solution.secondHighest("dfa12321afd") << endl;
+	cout << solution.secondHighest("abc1111") << endl;
+}
