7a6fab215f3cd64be7c4ef573776fdecdb3824c6
TransouL <tran_soul@163.com>
2022-12-08 00:55:16 +0800

    1812-determine-color-of-a-chessboard-square.cpp

---
+++ 1812-determine-color-of-a-chessboard-square.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+	bool squareIsWhite(string coordinates) {
+		return ((coordinates[0] - 'a') ^ (coordinates[1] - '1')) & 1;
+	}
+};
+
+int main(void) {
+	Solution solution = Solution();
+	cout << solution.squareIsWhite("a1") << endl;
+	cout << solution.squareIsWhite("h3") << endl;
+	cout << solution.squareIsWhite("c7") << endl;
+}
