904118d3ede89870950cb82aeea961d319d2917d
TransouL <tran_soul@163.com>
2022-12-09 00:34:08 +0800

    1780-check-if-number-is-a-sum-of-powers-of-three.cpp

---
+++ 1780-check-if-number-is-a-sum-of-powers-of-three.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+	bool checkPowersOfThree(int n) {
+		while (n) {
+			if (n % 3 == 0 || n % 3 == 1) {
+				n /= 3;
+			}
+			else {
+				return false;
+			}
+		}
+		return true;
+	}
+};
+
+int main(void) {
+	Solution solution = Solution();
+	cout << solution.checkPowersOfThree(12) << endl;
+	cout << solution.checkPowersOfThree(91) << endl;
+	cout << solution.checkPowersOfThree(21) << endl;
+}
