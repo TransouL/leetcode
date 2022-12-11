1ee2c8421f204bc70808cafff970695198be4a1e
TransouL <tran_soul@163.com>
2022-12-12 01:31:00 +0800

    1781-sum-of-beauty-of-all-substrings.cpp

---
+++ 1781-sum-of-beauty-of-all-substrings.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+	int beautySum(string s) {
+		int ans = 0, n = s.size();
+		int max_, min_;
+		for (int i = 0; i < n; i++) {
+			vector<int> cnt(26);
+			for (int j = i; j < n; j++) {
+				++cnt[s[j] - 'a'];
+				if (j > i + 1) {
+					max_ = 0, min_ = 501;
+					for (auto &c : cnt) {
+						if (c) {
+							max_ = max(max_, c);
+							min_ = min(min_, c);
+						}
+					}
+					ans += max_ - min_;
+				}
+			}
+		}
+		return ans;
+	}
+};
+
+int main(void) {
+	Solution solution = Solution();
+	cout << solution.beautySum("aabcb") << endl;
+	cout << solution.beautySum("aabcbaa") << endl;
+}
