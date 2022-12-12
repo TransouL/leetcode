95813c2e24402833483c1cbd99ca57ee0151bd76
TransouL <tran_soul@163.com>
2022-12-13 07:16:36 +0800

    1832-check-if-the-sentence-is-pangram.cpp

---
+++ 1832-check-if-the-sentence-is-pangram.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+	bool checkIfPangram(string sentence) {
+		bool stat[26] = {false};
+		for (auto &ch : sentence) {
+			stat[ch - 'a'] = true;
+		}
+		for (int i = 0; i < 26; i++) {
+			if (!stat[i]) {
+				return false;
+			}
+		}
+		return true;
+	}
+};
+
+int main(void) {
+	Solution solution = Solution();
+	cout << solution.checkIfPangram("thequickbrownfoxjumpsoverthelazydog") << endl;
+	cout << solution.checkIfPangram("leetcode") << endl;
+}
