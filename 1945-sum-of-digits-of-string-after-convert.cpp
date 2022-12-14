f674b6aaab1be28c3222a92e5c639efaa43168b5
TransouL <tran_soul@163.com>
2022-12-15 06:18:02 +0800

    1945-sum-of-digits-of-string-after-convert.cpp

---
+++ 1945-sum-of-digits-of-string-after-convert.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+	int getLucky(string s, int k) {
+		string ans_str = "";
+		for (auto &c : s) {
+			ans_str += to_string(c - 'a' + 1);
+		}
+		int ans;
+		while (k-- > 0) {
+			ans = 0;
+			for (auto &c : ans_str) {
+				ans += (c - '0');
+			}
+			ans_str = to_string(ans);
+		}
+		return ans;
+	}
+};
+
+int main(void) {
+	Solution solution = Solution();
+	cout << solution.getLucky("iiii", 1) << endl;
+	cout << solution.getLucky("leetcode", 2) << endl;
+}
