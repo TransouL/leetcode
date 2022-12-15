cf680c442fdf8b24a880e72546ce1b84dc62ed26
TransouL <tran_soul@163.com>
2022-12-16 06:00:20 +0800

    1785-minimum-elements-to-add-to-form-a-given-sum.cpp

---
+++ 1785-minimum-elements-to-add-to-form-a-given-sum.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+	int minElements(vector<int>& nums, int limit, int goal) {
+		long diff = goal;
+		for (auto &num : nums) {
+			diff -= num;
+		}
+		return diff == 0 ? 0 : (abs(diff) - 1) / limit + 1;
+	}
+};
+
+int main(void) {
+	Solution solution = Solution();
+	cout << solution.minElements(vector<int> {1, -1, 1}, 3, -4) << endl;
+	cout << solution.minElements(vector<int> {1, -10, 9, 1}, 100, 0) << endl;
+	cout << solution.minElements(vector<int> {1, 2, 3}, 300, 6) << endl;
+}
