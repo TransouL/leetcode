96525c37a38b0b20cf13d4cda328bc7d6fea14c1
TransouL <tran_soul@163.com>
2022-12-21 19:34:52 +0800

    1753-maximum-score-from-removing-stones.cpp

---
+++ 1753-maximum-score-from-removing-stones.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+	int maximumScore(int a, int b, int c) {
+		int s = a, m = b, l;
+		if (a > b) {
+			s = b;
+			m = a;
+		}
+		if (c <= s) {
+			l = m;
+			m = s;
+			s = c;
+		}
+		else if (c < m){
+			l = m;
+			m = c;
+		}
+		else {
+			l = c;
+		}
+
+		if (s + m <= l) {
+			return s + m;
+		}
+		else {
+			return l + s - (l - m + s + 1) / 2;
+		}
+	}
+};
+
+int main(void) {
+	Solution solution = Solution();
+	cout << solution.maximumScore(2, 4, 6) << endl;
+	cout << solution.maximumScore(4, 4, 6) << endl;
+	cout << solution.maximumScore(1, 8, 8) << endl;
+}
