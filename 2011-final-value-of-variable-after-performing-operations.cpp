ec04ebd6bcc65d8766576f02705e497956ff19c7
TransouL <tran_soul@163.com>
2022-12-23 18:17:59 +0800

    2011-final-value-of-variable-after-performing-operations.cpp

---
+++ 2011-final-value-of-variable-after-performing-operations.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+	int finalValueAfterOperations(vector<string>& operations) {
+		int ans = 0;
+		for (auto &op : operations) {
+			if (op[1] == '+') {
+				++ans;
+			}
+			else {
+				--ans;
+			}
+		}
+		return ans;
+	}
+};
+
+int main(void) {
+	Solution solution = Solution();
+	cout << solution.finalValueAfterOperations(vector<string> {"--X", "X++", "X++"}) << endl;
+	cout << solution.finalValueAfterOperations(vector<string> {"++X", "++X", "X++"}) << endl;
+	cout << solution.finalValueAfterOperations(vector<string> {"X++", "++X", "--X", "X--"}) << endl;
+}
