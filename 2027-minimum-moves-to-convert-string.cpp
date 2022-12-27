8ed8baadb466b4ff9b28408d2e47d11630c32cdd
TransouL <tran_soul@163.com>
2022-12-27 13:56:29 +0800

    2027-minimum-moves-to-convert-string.cpp

---
+++ 2027-minimum-moves-to-convert-string.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+	int minimumMoves(string s) {
+		int ans = 0, i = 0;
+		while (i < s.size()) {
+			if (s[i] == 'X') {
+				++ans;
+				i += 3;
+			}
+			else {
+				++i;
+			}
+		}
+		return ans;
+	}
+};
+
+int main(void) {
+	Solution solution = Solution();
+	cout << solution.minimumMoves("XXX") << endl;
+	cout << solution.minimumMoves("XXOX") << endl;
+	cout << solution.minimumMoves("OOOO") << endl;
+}
