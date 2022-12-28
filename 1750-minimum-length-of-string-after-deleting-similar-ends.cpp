f2641b71b76f85dfbaafb771ba146018b09ab51d
TransouL <tran_soul@163.com>
2022-12-28 12:49:13 +0800

    1750-minimum-length-of-string-after-deleting-similar-ends.cpp

---
+++ 1750-minimum-length-of-string-after-deleting-similar-ends.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+	int minimumLength(string s) {
+		int l = 0, r = s.size() - 1;
+		while (true) {
+			if (l > r) {
+				return 0;
+			}
+			if (l == r) {
+				return 1;
+			}
+			char lc = s[l], rc = s[r];
+			if (lc != rc) {
+				break;
+			}
+			else {
+				while (l <= r && s[l] == lc) {
+					++l;
+				}
+				while (r >= l && s[r] == rc) {
+					--r;
+				}
+			}
+		}
+		return r - l + 1;
+	}
+};
+
+int main(void) {
+	Solution solution = Solution();
+	cout << solution.minimumLength("ca") << endl;
+	cout << solution.minimumLength("cabaabac") << endl;
+	cout << solution.minimumLength("aabccabba") << endl;
+	cout << solution.minimumLength("bbbbbbbbbbbbbbbbbbbbbbbbbbbabbbbbbbbbbbbbbbccbcbcbccbbabbb") << endl; // 1
+	cout << solution.minimumLength("bbbbbbbbbbbbbbbbbbb") << endl;
+}
