b3015f851562e1e5e554daf2775ac67a19581e39
TransouL <tran_soul@163.com>
2022-12-31 13:26:10 +0800

    2037-minimum-number-of-moves-to-seat-everyone.cpp

---
+++ 2037-minimum-number-of-moves-to-seat-everyone.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+	int minMovesToSeat(vector<int>& seats, vector<int>& students) {
+		sort(seats.begin(), seats.end());
+		sort(students.begin(), students.end());
+		int ans = 0;
+		for (int i = 0; i < seats.size(); i++) {
+			ans += abs(seats[i] - students[i]);
+		}
+		return ans;
+	}
+};
+
+int main(void) {
+	Solution solution = Solution();
+	cout << solution.minMovesToSeat(vector<int>{3, 1, 5}, vector<int>{2, 7, 4}) << endl;
+	cout << solution.minMovesToSeat(vector<int>{12, 14, 19, 19, 12}, vector<int>{19, 2, 17, 20, 7}) << endl; // 19
+}
