4558890e8297c25ac5b9444144a5edf9694cd599
TransouL <tran_soul@163.com>
2023-01-01 22:08:29 +0800

    2351-first-letter-to-appear-twice.cpp

---
+++ 2351-first-letter-to-appear-twice.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+	char repeatedCharacter(string s) {
+		bool flag[26] = { false };
+		for (auto &ch : s) {
+			int idx = ch - 'a';
+			if (flag[idx]) {
+				return ch;
+			}
+			else {
+				flag[idx] = true;
+			}
+		}
+		return 0;
+	}
+};
+
+int main(void) {
+	Solution solution = Solution();
+	cout << solution.repeatedCharacter("abccbaacz") << endl;
+	cout << solution.repeatedCharacter("abcdd") << endl;
+}
