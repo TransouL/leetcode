0e1dbdd5375cd5b682fb3e24e183717838ea0d12
TransouL <tran_soul@163.com>
2023-01-09 01:07:20 +0800

    1806-minimum-number-of-operations-to-reinitialize-a-permutation.cpp

---
+++ 1806-minimum-number-of-operations-to-reinitialize-a-permutation.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+	int reinitializePermutation(int n) {
+		if (n == 2) {
+			return 1;
+		}
+		int step = 1, pow2 = 2;
+		while (pow2 != 1) {
+			++step;
+			pow2 = pow2 * 2 % (n - 1);
+		}
+		return step;
+	}
+};
+
+int main(void) {
+	Solution solution = Solution();
+	cout << solution.reinitializePermutation(2) << endl;
+	cout << solution.reinitializePermutation(4) << endl;
+	cout << solution.reinitializePermutation(6) << endl;
+}
