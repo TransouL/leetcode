2e643e629bd5ae3536290ec427aae3aac679b991
TransouL <tran_soul@163.com>
2023-01-11 00:36:56 +0800

    2283-check-if-number-has-equal-digit-count-and-digit-value.cpp

---
+++ 2283-check-if-number-has-equal-digit-count-and-digit-value.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+	bool digitCount(string num) {
+		int n = num.size();
+		int stats[10] = { 0 };
+		for (auto &ch : num) {
+			++stats[ch - '0'];
+		}
+		for (int i = 0; i < n; i++) {
+			if (stats[i] != (num[i] - '0')) {
+				return false;
+			}
+		}
+		return true;
+	}
+};
+
+int main(void) {
+	Solution solution = Solution();
+	cout << solution.digitCount("1210") << endl;
+	cout << solution.digitCount("030") << endl;
+}
