a65678d60f1a397c4e1f0533f607ab11938758a6
TransouL <tran_soul@163.com>
2023-01-12 00:54:57 +0800

    1807-evaluate-the-bracket-pairs-of-a-string.cpp

---
+++ 1807-evaluate-the-bracket-pairs-of-a-string.cpp
@@ -0,0 +1,43 @@
+﻿#include <iostream>
+#include <vector>
+#include <string>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+public:
+	string evaluate(string s, vector<vector<string>>& knowledge) {
+		unordered_map<string, string> mapping;
+		for (auto &k : knowledge) {
+			mapping.emplace(k[0], k[1]);
+		}
+		string ans = "";
+		int left = -1;
+		for (int i = 0; i < s.size(); i++) {
+			if (s[i] == '(') {
+				left = i;
+			}
+			else if (s[i] == ')') {
+				string key = s.substr(left + 1, i - left - 1);
+				if (mapping.count(key)) {
+					ans += mapping[key];
+				}
+				else {
+					ans += '?';
+				}
+				left = -1;
+			}
+			else if (left == -1) {
+				ans += s[i];
+			}
+		}
+		return ans;
+	}
+};
+
+int main(void) {
+	Solution solution = Solution();
+	cout << solution.evaluate("(name)is(age)yearsold", vector<vector<string>>{{"name", "bob"}, { "age", "two" }}) << endl;
+	cout << solution.evaluate("hi(name)", vector<vector<string>>{{ "a", "b" }}) << endl;
+	cout << solution.evaluate("(a)(a)(a)aaa", vector<vector<string>>{{"a", "yes"}}) << endl;
+}
