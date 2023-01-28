71dd99a137ee1e94d7047f783f8d397bae6a5972
TransouL <tran_soul@163.com>
2023-01-29 05:55:32 +0800

    2315-count-asterisks.cpp

---
+++ 2315-count-asterisks.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+	int countAsterisks(string s) {
+		int ans = 0;
+		bool check = true;
+		for (auto &ch : s) {
+			if (ch == '|') {
+				check = !check;
+			} else if (check && ch == '*') {
+				++ans;
+			}
+		}
+		return ans;
+	}
+};
+
+int main(void) {
+	Solution solution = Solution();
+	cout << solution.countAsterisks("l|*e*et|c**o|*de|") << endl;
+	cout << solution.countAsterisks("iamprogrammer") << endl;
+	cout << solution.countAsterisks("yo|uar|e**|b|e***au|tifu|l") << endl;
+}
