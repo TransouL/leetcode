6091221627187cf0b594e4d89a5bbcd68480eb96
TransouL <tran_soul@163.com>
2023-02-04 01:55:41 +0800

    1798-maximum-number-of-consecutive-values-you-can-make.cpp

---
+++ 1798-maximum-number-of-consecutive-values-you-can-make.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+	int getMaximumConsecutive(vector<int>& coins) {
+		sort(coins.begin(), coins.end());
+		int ans = 0;
+		for (auto &c : coins) {
+			if (c <= ans + 1) {
+				ans += c;
+			}
+			else {
+				break;
+			}
+		}
+		return ans + 1;
+	}
+};
+
+int main(void) {
+	Solution solution = Solution();
+	cout << solution.getMaximumConsecutive(vector<int> {1, 3}) << endl;
+	cout << solution.getMaximumConsecutive(vector<int> {1, 1, 1, 4}) << endl;
+	cout << solution.getMaximumConsecutive(vector<int> {1, 4, 10, 3, 1}) << endl;
+}
