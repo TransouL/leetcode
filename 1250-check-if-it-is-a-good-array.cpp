3bea9fb92c7e4abb55120bb7ffd733dbcf2eb706
TransouL <tran_soul@163.com>
2023-02-15 00:53:29 +0800

    1250-check-if-it-is-a-good-array.cpp

---
+++ 1250-check-if-it-is-a-good-array.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+	int gcd(int a, int b) {
+		return b != 0 ? gcd(b, a % b) : a;
+	}
+
+public:
+	bool isGoodArray(vector<int>& nums) {
+		int divisor = nums[0];
+		for (auto &num : nums) {
+			divisor = gcd(divisor, num);
+			if (divisor == 1) {
+				return true;
+			}
+		}
+		return false;
+	}
+};
+
+int main(void) {
+	Solution solution = Solution();
+	cout << solution.isGoodArray(vector<int> {12, 5, 7, 23}) << endl;
+	cout << solution.isGoodArray(vector<int> {29, 6, 10}) << endl;
+	cout << solution.isGoodArray(vector<int> {3, 6}) << endl;
+}
