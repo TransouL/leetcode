760f26590f8476618c5c4f745eb7d48e257c6f25
TransouL <tran_soul@163.com>
2023-02-23 02:13:02 +0800

    1238-circular-permutation-in-binary-representation.cpp

---
+++ 1238-circular-permutation-in-binary-representation.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+	vector<int> circularPermutation(int n, int start) {
+		vector<int> ans(1 << n);
+		for (int i = 0; i < ans.size(); i++) {
+			ans[i] = (i >> 1) ^ i ^ start;
+		}
+		return ans;
+	}
+};
+
+template<typename T>
+void printVector(const vector<T> &v){
+	for (auto &e : v) cout << e << ", ";
+	cout << endl;
+}
+
+int main(void) {
+	Solution solution = Solution();
+	printVector(solution.circularPermutation(2, 3));
+	printVector(solution.circularPermutation(3, 2));
+}
