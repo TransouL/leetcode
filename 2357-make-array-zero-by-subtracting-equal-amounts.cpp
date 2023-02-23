850492732cefa7883524388d05ad2faecd394ebb
TransouL <tran_soul@163.com>
2023-02-24 04:53:50 +0800

    2357-make-array-zero-by-subtracting-equal-amounts.cpp

---
+++ 2357-make-array-zero-by-subtracting-equal-amounts.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <vector>
+#include <unordered_set>
+using namespace std;
+
+class Solution {
+public:
+	int minimumOperations(vector<int>& nums) {
+		unordered_set<int> st;
+		for (auto &num : nums) {
+			if (num) {
+				st.emplace(num);
+			}
+		}
+		return st.size();
+	}
+};
+
+int main(void) {
+	Solution solution = Solution();
+	cout << solution.minimumOperations(vector<int> {1, 5, 0, 3, 5}) << endl;
+}
