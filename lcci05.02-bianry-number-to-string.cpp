8574334e5785b54dcc027141dc76b2ae44d9ca6f
TransouL <tran_soul@163.com>
2023-03-02 00:34:49 +0800

    lcci05.02-bianry-number-to-string.cpp

---
+++ lcci05.02-bianry-number-to-string.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+	string printBin(double num) {
+		string ans = "0.";
+		while (num != 0 && ans.size() <= 32) {
+			num *= 2;
+			int digit = num;
+			ans += ('0' + digit);
+			num -= digit;
+		}
+		return ans.size() <= 32 ? ans : "ERROR";
+	}
+};
+
+int main(void) {
+	Solution solution = Solution();
+	cout << solution.printBin(0.625) << endl;
+	cout << solution.printBin(0.1) << endl;
+}
