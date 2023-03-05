fa4dd0a84a7105b672d88300d12be49de23014a4
TransouL <tran_soul@163.com>
2023-03-06 00:55:27 +0800

    1653-minimum-deletions-to-make-string-balanced.cpp

---
+++ 1653-minimum-deletions-to-make-string-balanced.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+	int minimumDeletions(string s) {
+		int n = s.size(), leftb = 0, righta = 0;
+		for (int i = 0; i < n; i++) {
+			if (s[i] == 'a') {
+				++righta;
+			}
+		}
+		int ans = righta;
+		for (int i = 0; i < n; i++) {
+			if (s[i] == 'a') {
+				--righta;
+			}
+			else {
+				++leftb;
+			}
+			ans = min(ans, leftb + righta);
+		}
+		return ans;
+	}
+};
+
+int main(void) {
+	Solution solution = Solution();
+	cout << solution.minimumDeletions("aababbab") << endl;
+	cout << solution.minimumDeletions("bbaaaaabb") << endl;
+	cout << solution.minimumDeletions("bbbabbb") << endl;
+}
