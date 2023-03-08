e84465392125fef95aaeba4e52dcdaecab584b75
TransouL <tran_soul@163.com>
2023-03-09 06:04:12 +0800

    2379-minimum-recolors-to-get-k-consecutive-black-blocks.cpp

---
+++ 2379-minimum-recolors-to-get-k-consecutive-black-blocks.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+	int minimumRecolors(string blocks, int k) {
+		int b = 0;
+		for (int i = 0; i < k; i++) {
+			if (blocks[i] == 'B') {
+				++b;
+			}
+		}
+		int ans = k - b;
+		for (int i = k; i < blocks.size(); i++) {
+			if (blocks[i] != blocks[i - k]) {
+				if (blocks[i] == 'B') {
+					++b;
+					ans = min(ans, k - b);
+					if (ans == 0) {
+						break;
+					}
+				}
+				else {
+					--b;
+				}
+			}
+		}
+		return ans;
+	}
+};
+
+int main(void) {
+	Solution solution = Solution();
+	cout << solution.minimumRecolors("WBBWWBBWBW", 7) << endl;
+	cout << solution.minimumRecolors("WBWBBBW", 2) << endl;
+}
