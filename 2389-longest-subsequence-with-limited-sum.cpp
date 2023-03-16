fd05f31c150032bf80b3f1577b9ce046599bb6ae
TransouL <tran_soul@163.com>
2023-03-17 02:49:37 +0800

    2389-longest-subsequence-with-limited-sum.cpp

---
+++ 2389-longest-subsequence-with-limited-sum.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+	vector<int> answerQueries(vector<int>& nums, vector<int>& queries) {
+		int n = nums.size(), m = queries.size();
+		sort(nums.begin(), nums.end());
+		vector<int> f(n + 1);
+		for (int i = 0; i < n; i++) {
+			f[i + 1] = f[i] + nums[i];
+		}
+		vector<int> ans(m);
+		for (int i = 0; i < m; i++) {
+			ans[i] = upper_bound(f.begin(), f.end(), queries[i]) - f.begin() - 1;
+		}
+		return ans;
+	}
+};
+
+template<typename T>
+void printVector(const vector<T> &v){
+	for (auto &e : v) cout << e << ", ";
+	cout << endl;
+}
+
+int main(void) {
+	Solution solution = Solution();
+	printVector(solution.answerQueries(vector<int>{4, 5, 2, 1}, vector<int>{3, 10, 21}));
+	printVector(solution.answerQueries(vector<int>{2, 3, 4, 5}, vector<int>{1}));
+}
