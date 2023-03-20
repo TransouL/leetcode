f63d22772879977e119feff30f9e8193bc85ffa9
TransouL <tran_soul@163.com>
2023-03-21 04:05:21 +0800

    2469-convert-the-temperature.cpp

---
+++ 2469-convert-the-temperature.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+	vector<double> convertTemperature(double celsius) {
+		return{ celsius + 273.15, celsius * 1.80 + 32.00 };
+	}
+};
+
+template<typename T>
+void printVector(const vector<T> &v){
+	for (auto &e : v) cout << e << ", ";
+	cout << endl;
+}
+
+int main(void) {
+	Solution solution = Solution();
+	printVector(solution.convertTemperature(36.50));
+	printVector(solution.convertTemperature(122.11));
+}
