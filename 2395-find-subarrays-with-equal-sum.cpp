6bc432a7466f03a5a3e7639e0e04f958dde57a25
TransouL <tran_soul@163.com>
2023-03-26 01:44:49 +0800

    2395-find-subarrays-with-equal-sum.cpp

---
+++ 2395-find-subarrays-with-equal-sum.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <vector>
+#include <unordered_set>
+using namespace std;
+
+class Solution {
+public:
+	bool findSubarrays(vector<int>& nums) {
+		int n = nums.size();
+		unordered_set<int> sums;
+		for (int i = 0; i < n - 1; i++) {
+			int t = nums[i] + nums[i + 1];
+			if (sums.count(t)) {
+				return true;
+			}
+			sums.emplace(t);
+		}
+		return false;
+	}
+};
+
+int main(void) {
+	Solution solution = Solution();
+	cout << solution.findSubarrays(vector<int>{4, 2, 4}) << endl;
+	cout << solution.findSubarrays(vector<int>{1, 2, 3, 4, 5}) << endl;
+	cout << solution.findSubarrays(vector<int>{0, 0, 0}) << endl;
+}
