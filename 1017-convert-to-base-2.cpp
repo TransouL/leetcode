9f3b6fdb6843983862dba6a51e89155d59ee69c6
TransouL <tran_soul@163.com>
2023-04-06 02:14:43 +0800

    1017-convert-to-base-2.cpp

---
+++ 1017-convert-to-base-2.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+	string baseNeg2(int n) {
+		if (n == 0 || n == 1) {
+			return to_string(n);
+		}
+		string ans;
+		while (n) {
+			int rem = n & 1;
+			ans.push_back('0' + rem);
+			n -= rem;
+			n /= -2;
+		}
+		reverse(ans.begin(), ans.end());
+		return ans;
+	}
+};
+
+int main(void) {
+	Solution solution = Solution();
+	cout << solution.baseNeg2(2) << endl;
+	cout << solution.baseNeg2(3) << endl;
+	cout << solution.baseNeg2(4) << endl;
+}
