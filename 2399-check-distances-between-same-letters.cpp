f5032f140394223711b65c994b74ec90239be869
TransouL <tran_soul@163.com>
2023-04-09 11:16:26 +0800

    2399-check-distances-between-same-letters.cpp

---
+++ 2399-check-distances-between-same-letters.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+	bool checkDistances(string s, vector<int>& distance) {
+		bool visited[26] = { false };
+		for (int i = 0; i < s.size(); i++) {
+			int idx = s[i] - 'a';
+			if (visited[idx]) {
+				if (distance[idx] != i) {
+					return false;
+				}
+			}
+			else {
+				distance[idx] += i + 1;
+				visited[idx] = true;
+			}
+		}
+		return true;
+	}
+};
+
+int main(void) {
+	Solution solution = Solution();
+	cout << solution.checkDistances("abaccb", vector<int>{1, 3, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}) << endl;
+	cout << solution.checkDistances("aa", vector<int>{1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0}) << endl;
+}
