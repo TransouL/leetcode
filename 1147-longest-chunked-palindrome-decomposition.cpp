84dc642b1a3e608a847686053c88be6b08c3b5a8
TransouL <tran_soul@163.com>
2023-04-12 07:25:40 +0800

    1147-longest-chunked-palindrome-decomposition.cpp

---
+++ 1147-longest-chunked-palindrome-decomposition.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+	int longestDecomposition(string text) {
+		int ans = 0;
+		while (!text.empty()) {
+			int i = 1, n = text.length();
+			while (i <= n / 2 && text.substr(0, i) != text.substr(n - i)) {
+				++i;
+			}
+			if (i > n / 2) {
+				++ans;
+				break;
+			}
+			ans += 2;
+			text = text.substr(i, n - i * 2);
+		}
+		return ans;
+	}
+};
+
+int main(void) {
+	Solution solution = Solution();
+	cout << solution.longestDecomposition("ghiabcdefhelloadamhelloabcdefghi") << endl;
+	cout << solution.longestDecomposition("merchant") << endl;
+	cout << solution.longestDecomposition("antaprezatepzapreanta") << endl;
+}
