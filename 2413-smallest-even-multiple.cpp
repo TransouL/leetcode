b42c605951fb59a8909281b09d80e5f05203f954
TransouL <tran_soul@163.com>
2023-04-21 05:58:46 +0800

    2413-smallest-even-multiple.cpp

---
+++ 2413-smallest-even-multiple.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+	int smallestEvenMultiple(int n) {
+		return n & 1 ? n * 2 : n;
+	}
+};
+
+int main(void) {
+	Solution solution = Solution();
+	cout << solution.smallestEvenMultiple(5) << endl;
+	cout << solution.smallestEvenMultiple(6) << endl;
+}
