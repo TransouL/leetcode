a7d1c35b1c9986f4842a7a803d612e05ecd7c8d3
TransouL <tran_soul@163.com>
2023-04-24 03:28:22 +0800

    1163-last-substring-in-lexicographical-order.cpp

---
+++ 1163-last-substring-in-lexicographical-order.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+	string lastSubstring(string s) {
+		int i = 0, j = 1, n = s.size();
+		while (j < n) {
+			int k = 0;
+			while (j + k < n && s[i + k] == s[j + k]) {
+				++k;
+			}
+			if (j + k < n && s[i + k] < s[j + k]) {
+				int t = i;
+				i = j;
+				j = max(j + 1, t + k + 1);
+			}
+			else {
+				j = j + k + 1;
+			}
+		}
+		return s.substr(i, n - i);
+	}
+};
+
+int main(void) {
+	Solution solution = Solution();
+	cout << solution.lastSubstring("abab") << endl;
+	cout << solution.lastSubstring("leetcode") << endl;
+}
