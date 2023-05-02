83468195c87ac822244d652fe745435dc5de6bd5
TransouL <tran_soul@163.com>
2023-05-03 01:56:06 +0800

    1003-check-if-word-is-valid-after-substitutions.cpp

---
+++ 1003-check-if-word-is-valid-after-substitutions.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+	bool isValid(string s) {
+		while (true) {
+			int i = s.find("abc");
+			if (i != string::npos) {
+				s = s.substr(0, i) + s.substr(i + 3);
+			}
+			else {
+				break;
+			}
+		}
+		return s == "";
+	}
+};
+
+int main(void) {
+	Solution solution = Solution();
+	cout << solution.isValid("aabcbc") << endl;
+	cout << solution.isValid("abcabcababcc") << endl;
+	cout << solution.isValid("abccba") << endl;
+}
