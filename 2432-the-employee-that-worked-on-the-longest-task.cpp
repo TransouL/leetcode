d6ace6f02b3a2c64ad34d32f384d7ea1a860a539
TransouL <tran_soul@163.com>
2023-05-05 07:11:17 +0800

    2432-the-employee-that-worked-on-the-longest-task.cpp

---
+++ 2432-the-employee-that-worked-on-the-longest-task.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+	int hardestWorker(int n, vector<vector<int>>& logs) {
+		int curr = 0, mx = 0, ans = n;
+		for (auto &log : logs) {
+			int dur = log[1] - curr, id = log[0];
+			if (dur > mx || (dur == mx && id < ans)) {
+				mx = dur;
+				ans = id;
+			}
+			curr = log[1];
+		}
+		return ans;
+	}
+};
+
+int main(void) {
+	Solution solution = Solution();
+	cout << solution.hardestWorker(10, vector<vector<int>>{{0, 3}, { 2, 5 }, { 0, 9 }, { 1, 15 }}) << endl;
+	cout << solution.hardestWorker(26, vector<vector<int>>{{1, 1}, { 3, 7 }, { 2, 12 }, { 7, 17 }}) << endl;
+	cout << solution.hardestWorker(2, vector<vector<int>>{{0, 10}, { 1, 20 }}) << endl;
+}
