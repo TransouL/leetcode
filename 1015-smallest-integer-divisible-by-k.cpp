725c6472559f923a7b722d87da20407e105d5d79
TransouL <tran_soul@163.com>
2023-05-10 07:39:57 +0800

    1015-smallest-integer-divisible-by-k.cpp

---
+++ 1015-smallest-integer-divisible-by-k.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+	int smallestRepunitDivByK(int k) {
+		if (k % 2 == 0 || k % 5 == 0)
+			return -1;
+		int x = 1 % k;
+		for (int i = 1; ; i++) {
+			if (x == 0)
+				return i;
+			x = (x * 10 + 1) % k;
+		}
+	}
+};
+
+int main(void) {
+	Solution solution = Solution();
+	cout << solution.smallestRepunitDivByK(1) << endl;
+	cout << solution.smallestRepunitDivByK(2) << endl;
+	cout << solution.smallestRepunitDivByK(3) << endl;
+}
