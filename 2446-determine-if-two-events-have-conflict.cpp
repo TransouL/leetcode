e6ea839f180a24481cf051c5db8282979b788568
TransouL <tran_soul@163.com>
2023-05-17 21:19:39 +0800

    2446-determine-if-two-events-have-conflict.cpp

---
+++ 2446-determine-if-two-events-have-conflict.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+	bool haveConflict(vector<string>& e1, vector<string>& e2) {
+		return e1[1] >= e2[0] && e2[1] >= e1[0];
+	}
+};
+
+int main(void) {
+	Solution solution = Solution();
+	cout << solution.haveConflict(vector<string>{"01:15", "02:00"}, vector<string>{"02:00", "03:00"}) << endl;
+	cout << solution.haveConflict(vector<string>{"01:00", "02:00"}, vector<string>{"01:20", "03:00"}) << endl;
+	cout << solution.haveConflict(vector<string>{"10:00", "11:00"}, vector<string>{"14:00", "15:00"}) << endl;
+}
