82bee719676261fab7d0a2d2b194ea4044379046
TransouL <tran_soul@163.com>
2023-05-31 06:20:44 +0800

    1130-minimum-cost-tree-from-leaf-values.cpp

---
+++ 1130-minimum-cost-tree-from-leaf-values.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <vector>
+#include <stack>
+using namespace std;
+
+class Solution {
+public:
+	int mctFromLeafValues(vector<int>& arr) {
+		int ans = 0;
+		stack<int> stk;
+		for (auto &x : arr) {
+			while (!stk.empty() && stk.top() <= x) {
+				int t = stk.top();
+				stk.pop();
+				if (stk.empty() || stk.top() > x) {
+					ans += t * x;
+				}
+				else {
+					ans += stk.top() * t;
+				}
+			}
+			stk.push(x);
+		}
+		while (stk.size() >= 2) {
+			int t = stk.top();
+			stk.pop();
+			ans += t * stk.top();
+		}
+		return ans;
+	}
+};
+
+int main(void) {
+	Solution solution = Solution();
+	cout << solution.mctFromLeafValues(vector<int> {6, 2, 4}) << endl;
+	cout << solution.mctFromLeafValues(vector<int> {4, 11}) << endl;
+}
