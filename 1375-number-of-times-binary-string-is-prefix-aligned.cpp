7eceb0ffaacae3bf481e1f03e59cf823db343b15
TransouL <tran_soul@163.com>
2023-06-14 00:44:57 +0800

    1375-number-of-times-binary-string-is-prefix-aligned.cpp

---
+++ 1375-number-of-times-binary-string-is-prefix-aligned.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+	int numTimesAllBlue(vector<int>& flips) {
+		int n = flips.size(), mx = -1, ans = 0;
+		for (int i = 0; i < n; i++) {
+			mx = max(mx, flips[i]);
+			if (mx == i + 1) {
+				++ans;
+				if (mx == n) {
+					break;
+				}
+			}
+		}
+		return ans;
+	}
+};
+
+int main(void) {
+	Solution solution = Solution();
+	cout << solution.numTimesAllBlue(vector<int>{3, 2, 4, 1, 5}) << endl;
+	cout << solution.numTimesAllBlue(vector<int>{4, 1, 2, 3}) << endl;
+}
