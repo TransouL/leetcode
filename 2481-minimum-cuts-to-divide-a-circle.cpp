6aaa24c8ea8a589c763a1ed7a9941b3a3f262b06
TransouL <tran_soul@163.com>
2023-06-17 08:03:01 +0800

    2481-minimum-cuts-to-divide-a-circle.cpp

---
+++ 2481-minimum-cuts-to-divide-a-circle.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+	int numberOfCuts(int n) {
+		if (n == 1)
+			return 0;
+		return n & 1 ? n : n / 2;
+	}
+};
+
+int main(void) {
+	Solution solution = Solution();
+	cout << solution.numberOfCuts(4) << endl;
+	cout << solution.numberOfCuts(3) << endl;
+	cout << solution.numberOfCuts(1) << endl; // 0
+}
